d1a965411b673ebd6a2be9af1021f417676d78fd
Mvppet30 <luisalonsoelizondo@gmail.com>
2023-11-06 17:56:44 -0600

    Arreglo Registro

---
+++ src/proyectoprogragrupo1/Cliente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoprogragrupo1;
+
+/**
+ *
+ * @author juanp
+ */
+public class Cliente {
+    String nombre;
+    String apellidos;
+    String identificacion;
+    String correo;
+    String numeroTelefonico;
+
+    public Cliente(String nombre, String apellidos, String identificacion, String correo, String numeroTelefonico) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.identificacion = identificacion;
+        this.correo = correo;
+        this.numeroTelefonico = numeroTelefonico;
+    }
+}
--- src/proyectoprogragrupo1/RegistroClientes.java
+++ src/proyectoprogragrupo1/RegistroClientes.java
@@ -4,10 +4,44 @@
  */
 package proyectoprogragrupo1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author juanp
  */
 public class RegistroClientes {
+    public static void registroClientes() {
+        Scanner scanner = new Scanner(System.in);
+        Cliente[] clientes = new Cliente[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese el nombre del cliente " + (i + 1) + ": ");
+            String nombre = scanner.nextLine();
+
+            System.out.println("Ingrese los apellidos del cliente " + (i + 1) + ": ");
+            String apellidos = scanner.nextLine();
+
+            System.out.println("Ingrese la identificación del cliente " + (i + 1) + ": ");
+            String identificacion = scanner.nextLine();
+
+            System.out.println("Ingrese el correo del cliente " + (i + 1) + ": ");
+            String correo = scanner.nextLine();
+
+            System.out.println("Ingrese el número telefónico del cliente " + (i + 1) + ": ");
+            String numeroTelefonico = scanner.nextLine();
+
+            clientes[i] = new Cliente(nombre, apellidos, identificacion, correo, numeroTelefonico);
+        }
+
+        System.out.println("Los clientes registrados son: ");
+        for (int i = 0; i < 5; i++) {
+            Cliente cliente = clientes[i];
+            System.out.println("Cliente " + (i + 1) + ": Nombre: " + cliente.nombre + " " + cliente.apellidos +
+                    ", Identificación: " + cliente.identificacion + ", Correo: " + cliente.correo +
+                    ", Número telefónico: " + cliente.numeroTelefonico);
+        }
+    }
+    
     
 }
